17169382164b950d27d69e1d3d2c84f88b3ded1e
rohankumar390 <rohanyt74@gmail.com>
2026-05-23 07:19:36 +0530

    Revert "Added sitemap and robots"
    This reverts commit 297a9e69c7f543b37de3246a989d63ea09c0def9.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,6 @@
 // src/app/about/page.tsx
 import OurFirm from "./our-firm/page";
 import LeadershipTeam from "@/src/app/about/leadership-team/page";
-import OurTeam from "@/src/app/about/our-team/page";
 import WhyChooseUs from "./WhyChooseUs/page";
 
 export const metadata = {
@@ -14,7 +13,7 @@ export default function AboutPage() {
   return (
     <div>
       <OurFirm />
-      <OurTeam />
+      <LeadershipTeam />
       <WhyChooseUs />
     </div>
   );
