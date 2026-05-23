0feae3189722f874743762b11d29c2803957d4dd
rohankumar390 <rohanyt74@gmail.com>
2026-05-23 07:24:53 +0530

    Added sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,27 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = 'https://www.rkssconsultants.com'
+
+  const routes = [
+    '',
+    '/about',
+    '/about/leadership-team',
+    '/about/our-firm',
+    '/about/WhyChooseUs',
+    '/contact',
+    '/legal/disclaimer',
+    '/legal/privacy-policy',
+    '/legal/terms-of-use',
+    '/resources/blog',
+    '/resources/tax-calendar',
+    '/services',
+  ]
+
+  return routes.map((route) => ({
+    url: `${baseUrl}${route}`,
+    lastModified: new Date(),
+    changeFrequency: 'weekly',
+    priority: route === '' ? 1 : 0.8,
+  }))
+}
