896fa797d3a6a59e5b759c15d8977ccf283dcc28
rohankumar390 <rohanyt74@gmail.com>
2026-05-30 19:52:55 +0530

    sitemap update

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,27 +1,28 @@
-import { MetadataRoute } from 'next'
+import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://www.rkssconsultants.com'
+  const baseUrl = "https://www.rkssconsultants.com";
 
   const routes = [
-    '',
-    '/about',
-    '/about/leadership-team',
-    '/about/our-firm',
-    '/about/WhyChooseUs',
-    '/contact',
-    '/legal/disclaimer',
-    '/legal/privacy-policy',
-    '/legal/terms-of-use',
-    '/resources/blog',
-    '/resources/tax-calendar',
-    '/services',
-  ]
+    "",
+    "/about",
+    "/about/leadership-team",
+    "/about/our-firm",
+    "/about/WhyChooseUs",
+    "/contact",
+    "/legal/disclaimer",
+    "/legal/privacy-policy",
+    "/legal/terms-of-use",
+    "/resources/blog",
+    "/resources/blog/companies-csr-policy-amendment-rules-2026",
+    "/resources/tax-calendar",
+    "/services",
+  ];
 
   return routes.map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date(),
-    changeFrequency: 'weekly',
-    priority: route === '' ? 1 : 0.8,
-  }))
+    changeFrequency: "weekly",
+    priority: route === "" ? 1 : 0.8,
+  }));
 }
